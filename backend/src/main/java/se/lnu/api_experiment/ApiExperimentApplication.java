a11b3b545d0bc38b18d2f09f82af1d1a6c552227
Henrik Mattsson <henkehenke@gmail.com>
2026-03-06 18:22:18 +0100

    Add basic Java Spring Boot skeleton for backend
    Just initialized a Java Spring Boot project through start.spring.io and
    unzipped it.

---
+++ backend/src/main/java/se/lnu/api_experiment/ApiExperimentApplication.java
@@ -0,0 +1,13 @@
+package se.lnu.api_experiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiExperimentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiExperimentApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/se/lnu/api_experiment/ApiExperimentApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lnu.api_experiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiExperimentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
